f3f8b8e35350d86b687bc1a5e2ab2801ab487b66
Alex Garnett <axfelix@gmail.com>
2019-01-04 11:12:07 -0800

    correct output, remove statscrunch.py, handle more data errors

--- ojsstats.py
+++ ojsstats.py
@@ -1,3 +1,16 @@
+"""OJS Stats.
+
+Usage:
+  ojsstats.py [--onlylogparse | --onlyharvest] [--logpath=<path>]
+
+Options:
+  --onlylogparse    Quit after parsing logs into the DB.
+  --onlyharvest     Don't parse logs; only harvest and export.
+  --logpath=<path>  The path to parse logs from. Defaults to /var/log/httpd.
+
+"""
+
+import os
 import re
 import sys
 import csv
@@ -5,6 +18,7 @@ import gzip
 import glob
 import urllib.request as urllib2 # forward-port from python 2
 import urllib.parse
+import urllib.error
 import json
 import time
 import random
@@ -13,13 +27,13 @@ import collections
 import sqlite3 as lite
 import datetime as dt
 import geoip2.database
+import geoip2.errors
 
+from docopt import docopt
 from tld import get_tld
 from sickle import Sickle
 from dateutil.parser import parse
 from xml.etree import ElementTree as ET
-
-litecon = lite.connect('data/ojs_oai.db')
 
 
 def find_country_in_title(journal, country_from_title, nat_to_iso3):
@@ -45,7 +59,7 @@ def find_journal_endpoint(oai_url, setSpec):
 	return journal_endpoint
 
 
-def find_oai_endpoint(url, is_beacon):
+def find_oai_endpoint(url, ip, is_beacon):
 	if is_beacon == False:
 		url = url.strip("/")
 		urls_to_try = []
@@ -102,7 +116,10 @@ def find_oai_endpoint(url, is_beacon):
 def verify_not_missing_journal(journal_endpoint, setSpec):
 	testing_url = (re.sub("verb=Identify", "verb=ListRecords&metadataPrefix=oai_dc&set=", journal_endpoint) + setSpec)
 	request = urllib2.Request(testing_url)
-	response = urllib2.urlopen(request, timeout=180)
+	try:
+		response = urllib2.urlopen(request, timeout=180)
+	except urllib.error.URLError:
+		return True
 	oai_xml = response.read()
 	if "noRecordsMatch" in str(oai_xml):
 		return True
@@ -175,11 +192,11 @@ def find_journal_contact(journal_endpoint):
 	return journal_contact
 
 
-def beacon_log_parser():
-	if len(sys.argv) > 1:
-		access_log_files = sys.argv[1]
-	else:
+def beacon_log_parser(logpath):
+	if not logpath:
 		access_log_files = '/var/log/httpd'
+	else:
+		access_log_files = logpath	
 	access_log_files = glob.glob(access_log_files + "/access*gz")
 
 	with gzip.open(access_log_files[-1]) as most_recent_log:
@@ -213,10 +230,10 @@ def beacon_log_parser():
 			oai_url = re.search('oai=(\S+?)\sHTTP', str(line))
 			if oai_url:
 				oai_url = oai_url.group(1)
-				oai_data = find_oai_endpoint(urllib.parse.unquote(oai_url), True)
+				oai_data = find_oai_endpoint(urllib.parse.unquote(oai_url), ip, True)
 			else:
 				oai_url = ""
-				oai_data = find_oai_endpoint("https://" + ip, False)
+				oai_data = find_oai_endpoint("https://" + ip, ip, False)
 
 			if not oai_data:
 				with open("data/checkOJSlog.txt", "a") as logfile:
@@ -230,16 +247,16 @@ def beacon_log_parser():
 					c = litecon.cursor()
 					try:
 						# disable any old IP addresses for this domain name
-						c.execute("UPDATE endpoints SET enabled=0 WHERE oai_url=? AND ip!=?", (oai_url, ip))
+						c.execute("UPDATE endpoints SET enabled=0 WHERE oai_url=? AND ip!=?", (re.sub("verb=Identify", "verb=identify", oai_url), ip))
 
-						c.execute("INSERT INTO endpoints (beacon_id, oai_url, repository_identifier, first_hit, last_hit, ip, version, enabled) VALUES(?,?,?,?,?,?,?,?)", (beacon_id, oai_url, repository_identifier, date_hit, date_hit, ip, ojs_version, 1))
+						c.execute("INSERT INTO endpoints (beacon_id, oai_url, repository_identifier, first_hit, last_hit, ip, version, enabled) VALUES(?,?,?,?,?,?,?,?)", (beacon_id, re.sub("verb=Identify", "verb=identify", oai_url), repository_identifier, date_hit, date_hit, ip, ojs_version, 1))
 
 					except lite.IntegrityError:
 						print("already had %s:%s:%s" % (repository_identifier, ip, beacon_id))
 
 						# check to see if it already exists without a beacon ID and update if we have one
 						c.execute('SELECT beacon_id FROM endpoints WHERE repository_identifier=? AND ip=?', (repository_identifier, ip))
-						if beacon_id and not c.fetchone()[0].encode:
+						if beacon_id and not c.fetchone()[0]:
 							c.execute("UPDATE endpoints SET beacon_id=? WHERE repository_identifier=? AND ip=?", (beacon_id, repository_identifier, ip))
 
 						# check if it's been upgraded and add to upgrades table
@@ -268,70 +285,18 @@ def beacon_log_parser():
 								journal_contact = find_journal_contact(journal_endpoint)
 
 							try:
-								c.execute("INSERT INTO journals (beacon_id, repository_identifier, ip, setSpec, setName, first_hit, last_hit, contact) VALUES(?,?,?,?,?,?,?,?)", (beacon_id, repository_identifier, ip, setSpec, setName, date_hit, date_hit, journal_contact))
+								c.execute("INSERT INTO journals (repository_identifier, ip, setSpec, setName, first_hit, last_hit, contact) VALUES(?,?,?,?,?,?,?)", (repository_identifier, ip, setSpec, setName, date_hit, date_hit, journal_contact))
 								archive_id = int(c.lastrowid)
-								# get and insert legacy "archive id" from old harvester codepaths; still used for lookups
-								c.execute("UPDATE journals SET archive_id=? WHERE beacon_id=? AND repository_identifier=? AND ip=? AND setSpec=?", (archive_id, beacon_id, repository_identifier, ip, setSpec))
+								# generate and insert legacy "archive id" from old harvester codepaths; still used for lookups
+								c.execute("UPDATE journals SET archive_id=? WHERE repository_identifier=? AND ip=? AND setSpec=?", (archive_id, repository_identifier, ip, setSpec))
 
 							except lite.IntegrityError:
 								print("Already had %s" % setSpec)
 								# on failed to insert, update the last hit date
-								c.execute("UPDATE journals SET last_hit=? WHERE beacon_id=? AND repository_identifier=? AND ip=? AND setSpec=?", (date_hit, beacon_id, repository_identifier, ip, setSpec))
-				except:
+								c.execute("UPDATE journals SET last_hit=? WHERE repository_identifier=? AND ip=? AND setSpec=?", (date_hit, repository_identifier, ip, setSpec))
+				except Exception as e:
 					with open("data/checkOJSlog.txt", "a") as logfile:
-						logfile.write("Something failed other than looking up URL for %s\n." % oai_url)
-
-
-def harvest():
-	with litecon:
-		c = litecon.cursor()
-		c2 = litecon.cursor()
-
-		dates_csv = open("data/dates.csv", "w")
-		writer = csv.DictWriter(dates_csv, fieldnames=["%03d" % a for a in range(1990,(int(time.strftime("%Y")) + 1))])
-		writer.writeheader()
-
-		for row in c.execute("SELECT beacon_id, repository_identifier, setSpec, setName, ip FROM journals"):
-			ojs = dict(zip(["beacon_id", "repository_identifier", "setSpec", "title", "ip"], row))
-
-			if ojs["title"] is None:
-				continue
-
-			c2.execute("SELECT oai_url FROM endpoints WHERE beacon_id=? AND repository_identifier=? AND ip=? AND enabled = 1", (ojs["beacon_id"], ojs["repository_identifier"], ojs["ip"]))
-			try: 
-				oai_url = c2.fetchone()[0]
-			except: 
-				continue
-
-			ojs["oai_endpoint"] = find_journal_endpoint(oai_url, ojs["setSpec"])
-
-			pub_years = []
-			# remove verb=Identify for sickle
-			sickle = Sickle(ojs["oai_endpoint"][:-14])
-			records = sickle.ListRecords(metadataPrefix="nlm", ignore_deleted=True)
-
-			while records:
-				try:
-					record = records.next()
-					for source in record.metadata["year"]:
-						match = re.search(r"(199[0-9]|20\d{2})", source)
-						if match:
-							year = match.group(0)
-						else:
-							year = parse(record.metadata["date"][0]).strftime('%Y')
-					pub_years.append(year)
-
-				except StopIteration:
-					break
-
-			year_count = collections.Counter()
-			for pub_year in pub_years:
-				year_count[pub_year] += 1
-
-			# TODO: this needs to have record_dates.columns = ['record_id', 'archive_id', 'dcdate', 'source']
-			writer.writerow(year_count)
-
-		dates_csv.close()
+						logfile.write("%s failed for  %s\n" % (e, oai_url))
 
 
 def country_lookup():
@@ -344,6 +309,10 @@ def country_lookup():
 
 	response = requests.get("http://api.worldbank.org/countries?per_page=400&format=json" )
 	wb_country_data = response.json()[1]
+
+	wb_country_income_map = {w['id'].lower(): w['incomeLevel']['id'] for w in wb_country_data}
+	# currently unused, carrying forward from old script
+	income_level_names = {w['incomeLevel']['id']: w['incomeLevel']['value'] for w in wb_country_data}
 
 	for country in wb_country_data:
 		if country['region']['value'] == 'Aggregates': continue
@@ -392,19 +361,19 @@ def country_lookup():
 
 	with litecon:
 		c = litecon.cursor()
-		c.execute("SELECT beacon_id, repository_identifier, setSpec, setName, archive_id, ip FROM journals")
+		c.execute("SELECT repository_identifier, setSpec, setName, archive_id, ip FROM journals")
 		journals = c.fetchall()
 
 		for journal in journals:
-			beacon_id, repository_identifier, setSpec, setName, archive_id, ip = journal
+			repository_identifier, setSpec, setName, archive_id, ip = journal
 
-			match = reader.country(ip)
-			if match is not None:
-				try:
-					journal_geoip = response.country.iso_code.lower()
-				except:
+			try:
+				match = reader.country(ip)
+				if match.country.names:
+					journal_geoip = match.country.names["en"].lower()
+				else:
 					journal_geoip = None
-			else:
+			except geoip2.errors.AddressNotFoundError:
 				journal_geoip = None
 
 			if setName is not None:
@@ -412,7 +381,7 @@ def country_lookup():
 			else:
 				country_in_title = None
 
-			c.execute("SELECT oai_url FROM endpoints WHERE beacon_id=? AND repository_identifier=? AND ip=?", (beacon_id, repository_identifier, ip))
+			c.execute("SELECT oai_url FROM endpoints WHERE repository_identifier=? AND ip=?", (repository_identifier, ip))
 			oai_url = c.fetchone()[0]
 			
 			# extract the TLD
@@ -465,25 +434,134 @@ def country_lookup():
 			except:
 				c.execute("UPDATE locales SET tld=?, country_in_title=?, geo_ip=?, country=?, region_id=?, region_name=? WHERE archive_id = ?", (tld, country_in_title, journal_geoip, country, region_id, region_name, archive_id))
 
+	return income_level_names
 
-if __name__ == "__main__":
+
+def harvest(income_level_names):
+	journals_per_year = collections.Counter()
+	articles_per_year = collections.Counter()
+	hosts_per_year = {k: [] for k in range(1990,(int(time.strftime("%Y")) + 1))}
+	sampled_years = ["%03d" % a for a in range(1990,(int(time.strftime("%Y")) + 1))]
 
 	with litecon:
 		c = litecon.cursor()
-		c.execute("CREATE TABLE IF NOT EXISTS endpoints (beacon_id TEXT, oai_url TEXT, repository_identifier TEXT, first_hit TEXT, last_hit TEXT, ip TEXT, version TEXT, enabled INTEGER)")
-		c.execute("CREATE UNIQUE INDEX IF NOT EXISTS id_plus_ip_plus_beacon ON endpoints (repository_identifier, ip, beacon_id)")
+		c2 = litecon.cursor()
 
-		c.execute("CREATE TABLE IF NOT EXISTS journals (beacon_id TEXT, repository_identifier TEXT, ip TEXT, setSpec TEXT, setName TEXT, first_hit TEXT, last_hit TEXT, contact TEXT, archive_id INTEGER)")
-		c.execute("CREATE UNIQUE INDEX IF NOT EXISTS id_plus_ip_plus_beacon_plus_spec ON journals (beacon_id, repository_identifier, ip, setSpec)")
+		dates_csv = open("data/all_articles_by_date.csv", "w", newline='')
+		dates_writer = csv.DictWriter(dates_csv, fieldnames=sampled_years)
+		dates_writer.writeheader()
+
+		countries_csv = open("data/journals_per_country.csv", "w", newline='')
+		countries_writer = csv.writer(countries_csv)
+		countries_writer.writerow(["year", "country", "region_id", "region_name", "archive_id"])
+
+		counts_csv = open("data/ojs_counts.csv", "w", newline='')
+		counts_writer = csv.writer(counts_csv)
+		counts_writer.writerow(["year", "journals", "articles", "hosts", "avgnumarts"])
+
+		for row in c.execute("SELECT repository_identifier, setSpec, setName, ip, archive_id FROM journals"):
+			ojs = dict(zip(["repository_identifier", "setSpec", "title", "ip", "archive_id"], row))
+
+			if ojs["title"] is None:
+				continue
+
+			c2.execute("SELECT oai_url, last_hit FROM endpoints WHERE repository_identifier=? AND ip=? AND enabled = 1", (ojs["repository_identifier"], ojs["ip"]))
+			try: 
+				oai_url, last_hit = c2.fetchone()
+			except: 
+				continue
+
+			delta = dt.datetime.today() - dt.datetime.strptime(last_hit, "%m/%d/%Y")
+			if delta.days > 30:
+				continue
+
+			ojs["oai_endpoint"] = find_journal_endpoint(oai_url, ojs["setSpec"])
+			if ojs["oai_endpoint"] is None:
+				continue
+
+			pub_years = []
+			# remove verb=Identify for sickle
+			sickle = Sickle(ojs["oai_endpoint"][:-14])
+			try:
+				records = sickle.ListRecords(metadataPrefix="nlm", ignore_deleted=True)
+			except:
+				continue
+
+			while records:
+				try:
+					try:
+						record = records.next()
+					except requests.exceptions.ConnectionError:
+						break
+
+					if "year" not in record.metadata.keys():
+						try:
+							year = parse(record.metadata["date"][0]).strftime('%Y')
+						except KeyError:
+							break
+					else:
+						match = re.search(r"(1\d{3}|20\d{2})", record.metadata["year"][0])
+						if match:
+							year = match.group(0)
+					pub_years.append(year)
+
+				except StopIteration:
+					break
+
+			year_count = collections.Counter()
+			for pub_year in pub_years:
+				if pub_year in sampled_years:
+					year_count[pub_year] += 1
+
+			dates_writer.writerow(year_count)
+
+			for pub_year in year_count:
+				if year_count[pub_year] >= 10:
+					articles_per_year[pub_year] += year_count[pub_year]
+					hosts_per_year[pub_year].append(oai_url)
+					journals_per_year[pub_year] += 1
+					c2.execute("SELECT country, region_id, region_name FROM locales WHERE archive_id=?", (ojs["archive_id"],))
+					locale = dict(zip(["country", "region_id", "region_name"], c2.fetchone()))
+					countries_writer.writerow([pub_year, locale["country"], locale["region_id"], locale["region_name"], ojs["archive_id"]])
+
+		dates_csv.close()
+		countries_csv.close()
+
+		for year in hosts_per_year:
+			hosts_per_year_counter[year] = len(set(hosts_per_year[year]))
+		for year in sampled_years:
+			counts_writer.writerow([year, journals_per_year[year], articles_per_year[year], hosts_per_year_counter[year], (articles_per_year[year]/journals_per_year[year])])
+		counts_csv.close()
+
+
+if __name__ == "__main__":
+	os.makedirs('data', exist_ok=True)
+	litecon = lite.connect('data/ojs_oai.db')
+	arguments = docopt(__doc__)
+
+	with litecon:
+		c = litecon.cursor()
+		# update old schema
+		try:
+			c.execute("ALTER TABLE endpoints ADD COLUMN beacon_id TEXT")
+			c.execute("ALTER TABLE upgrades ADD COLUMN beacon_id TEXT")
+		except:
+			pass
+		c.execute("CREATE TABLE IF NOT EXISTS endpoints (beacon_id TEXT, oai_url TEXT, repository_identifier TEXT, first_hit TEXT, last_hit TEXT, ip TEXT, version TEXT, enabled INTEGER)")
+		c.execute("CREATE UNIQUE INDEX IF NOT EXISTS id_plus_ip ON endpoints (repository_identifier, ip)")
+		c.execute("CREATE UNIQUE INDEX IF NOT EXISTS beacon ON endpoints (beacon_id)")
+
+		c.execute("CREATE TABLE IF NOT EXISTS journals (repository_identifier TEXT, ip TEXT, setSpec TEXT, setName TEXT, first_hit TEXT, last_hit TEXT, contact TEXT, archive_id INTEGER)")
+		c.execute("CREATE UNIQUE INDEX IF NOT EXISTS id_plus_ip_plus_spec ON journals (repository_identifier, ip, setSpec)")
 
 		c.execute("CREATE TABLE IF NOT EXISTS upgrades (beacon_id TEXT, repository_identifier TEXT, ip TEXT, oldversion TEXT, newversion TEXT, date_hit TEXT)")
-		c.execute("CREATE UNIQUE INDEX IF NOT EXISTS id_plus_ip_plus_beacon_plus_version ON upgrades (beacon_id, repository_identifier, ip, newversion)")
 		
 		c.execute("CREATE TABLE IF NOT EXISTS locales (archive_id TEXT, tld TEXT, country_in_title TEXT, geo_ip TEXT, country TEXT, region_id TEXT, region_name TEXT)")
 		c.execute("CREATE UNIQUE INDEX IF NOT EXISTS locale_index ON locales (archive_id)")
 
-	beacon_log_parser()
-	harvest()
-	country_lookup()
-
-	# TODO: statscrunch currently expects data/archive_settings.csv with lastIndexedDate, recordCount, harvesterUrl
+	if not arguments["--onlyharvest"]:
+		beacon_log_parser(arguments["--logpath"])
+	if arguments["--onlylogparse"]:
+		quit()
+	income_level_names = country_lookup()
+	harvest(income_level_names)
